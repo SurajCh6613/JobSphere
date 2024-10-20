039daade24958270d95ef8437c690a9f2911d39f
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2024-10-20 17:57:11 +0530

    updatePassword function added

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,11 +1,12 @@
 import { catchAsyncError } from "../middlewares/catchAsyncError.js";
-import ErrorHandler, {errorMiddleware } from "../middlewares/error.js";
+import ErrorHandler, { errorMiddleware } from "../middlewares/error.js";
 import { User } from "../models/userSchema.js";
 import { v2 as cloudinary } from "cloudinary";
-import {sendToken} from "../utils/jwtTokens.js"
-import { setDriver } from "mongoose";
+import { sendToken } from "../utils/jwtTokens.js";
+import { setDriver, syncIndexes } from "mongoose";
 
 
+// User Register function
 export const register = catchAsyncError(async (req, res, next) => {
   try {
     const {
@@ -56,8 +57,8 @@ export const register = catchAsyncError(async (req, res, next) => {
         try {
           const cloudinaryResponse = await cloudinary.uploader.upload(
             resume.tempFilePath,
-            { folder: "Job_Seekers_Resume"}
-          )
+            { folder: "Job_Seekers_Resume" }
+          );
           if (!cloudinaryResponse || cloudinaryResponse.error) {
             return next(
               new ErrorHandler("Failed to upload resume to cloud", 500)
@@ -68,115 +69,141 @@ export const register = catchAsyncError(async (req, res, next) => {
             url: cloudinaryResponse.secure_url,
           };
           // console.log(cloudinaryResponse.secure_url);
-          
         } catch (error) {
           return next(new ErrorHandler("Failed to upload resume", 500));
         }
       }
     }
     const user = await User.create(userData);
-    sendToken(user,201,res,'User Registered')
+    sendToken(user, 201, res, "User Registered");
   } catch (error) {
     next(error);
   }
 });
 
-
 // User Login Method
-export const login = catchAsyncError(async(req,res,next)=>{
-  const {role,email,password} = req.body; // getting user Data
+export const login = catchAsyncError(async (req, res, next) => {
+  const { role, email, password } = req.body; // getting user Data
   // Checking for if user not provide
-  if(!role || !email || !password){
-    return next(new ErrorHandler('Email, Password, and Role are required.',400))
+  if (!role || !email || !password) {
+    return next(
+      new ErrorHandler("Email, Password, and Role are required.", 400)
+    );
   }
   // Getting User Password via checking with email - User exist or not
-  const user = await User.findOne({email}).select("+password");
-  if(!user){
-    return next(new ErrorHandler("Invalid email or Password. From Email",400))
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or Password. From Email", 400));
   }
   // Checking for password match
   const isPasswordMatched = await user.comparePassword(password);
-  if(!isPasswordMatched){
-    return next(new ErrorHandler("Invalid email or Password. From Password",400))
+  if (!isPasswordMatched) {
+    return next(
+      new ErrorHandler("Invalid email or Password. From Password", 400)
+    );
   }
   //Checking for Role
-  if(user.role !== role){
-    return next(new ErrorHandler("Invalid User role.",400))
+  if (user.role !== role) {
+    return next(new ErrorHandler("Invalid User role.", 400));
   }
 
   // If Everything good then login the user
-  sendToken(user,200,res,"User Logged in Successfully")
-})
-
+  sendToken(user, 200, res, "User Logged in Successfully");
+});
 
 // Logout Function
-export const logout = catchAsyncError(async(req,res,next)=>{
-  res.status(200).cookie("token","",{
-    expires:new Date(Date.now()),
-    httpOnly:true
-  }).json({
-    success:true,
-    message:"Logged out successfully"
-  })
-})
+export const logout = catchAsyncError(async (req, res, next) => {
+  res
+    .status(200)
+    .cookie("token", "", {
+      expires: new Date(Date.now()),
+      httpOnly: true,
+    })
+    .json({
+      success: true,
+      message: "Logged out successfully",
+    });
+});
 
 // Funtion to get user - user getting its details
-export const getUser = catchAsyncError(async(req,res,next)=>{
-    const user = req.user;
-    res.status(200).json({
-      success:true,
-      user
-    })
-})
+export const getUser = catchAsyncError(async (req, res, next) => {
+  const user = req.user;
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
 
 // Function to update User profile
-export const updateProfile = catchAsyncError(async(req,res,next)=>{
+export const updateProfile = catchAsyncError(async (req, res, next) => {
   const newUserdata = {
-    name:req.body.name,
-    email:req.body.email,
-    phone:req.body.phone,
-    address:req.body.address,
-    coverLetter:req.body.coverLetter,
-    niches:{
-      firstNiche:req.body.firstNiche,
-      secondNiche:req.body.secondNiche,
-      thirdNiche:req.body.thirdNiche,
-    }
-  }
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    address: req.body.address,
+    coverLetter: req.body.coverLetter,
+    niches: {
+      firstNiche: req.body.firstNiche,
+      secondNiche: req.body.secondNiche,
+      thirdNiche: req.body.thirdNiche,
+    },
+  };
 
-  const {firstNiche,secondNiche,thirdNiche} = newUserdata.niches;
+  const { firstNiche, secondNiche, thirdNiche } = newUserdata.niches;
 
-  if(req.user.role === "Job Seeker" && (!firstNiche || !secondNiche || !thirdNiche)){
-    return next(new ErrorHandler("Please Provide all Your job Niches.",400))
+  if (
+    req.user.role === "Job Seeker" &&
+    (!firstNiche || !secondNiche || !thirdNiche)
+  ) {
+    return next(new ErrorHandler("Please Provide all Your job Niches.", 400));
   }
 
   // Resume updation
-  if(req.files){
+  if (req.files) {
     const resume = req.files.resume;
-    if(resume){
+    if (resume) {
       const currentResumeId = req.user.resume.public_id;
-      if(currentResumeId){
+      if (currentResumeId) {
         await cloudinary.uploader.destroy(currentResumeId);
       }
-      const newResume = await cloudinary.uploader.upload(resume.tempFilePath,{
-        folder:"Job_Seekers_Resume"
+      const newResume = await cloudinary.uploader.upload(resume.tempFilePath, {
+        folder: "Job_Seekers_Resume",
       });
       newUserdata.resume = {
         public_id: newResume.public_id,
-        url:newResume.secure_url
-      }
+        url: newResume.secure_url,
+      };
     }
   }
 
-  const user = await User.findByIdAndUpdate(req.user.id,newUserdata,{
-    new:true,
-    runValidators:true,
-    useFindAndModify:false
-  })
+  const user = await User.findByIdAndUpdate(req.user.id, newUserdata, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
   res.status(200).json({
-    success:true,
+    success: true,
     user,
-    message:"Profile Updated"
-  })
+    message: "Profile Updated",
+  });
+});
 
-})
+// Update password function
+export const updatePassword = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select("+password");
+  // Checking old password matching or not
+  const isPasswordMatched = await user.comparePassword(req.body.oldPassword);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Old Password is incorrect.", 400));
+  }
+  // Checking new password and confirm password is matching or not
+  if (req.body.newPassword !== req.body.confirmPassword) {
+    return next(
+      new ErrorHandler("New Password and confirm password do not match.", 400)
+    );
+  }
+
+  user.password = req.body.newPassword;
+  await user.save();
+  sendToken(user, 200, res, "Password Updated Successfully");
+});
